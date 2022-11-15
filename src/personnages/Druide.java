7ae299f711f0c81168daa40fd6f47672301dfffe
Roua affes <Roua affes@172.17.153.15>
2022-11-15 15:03:48 +0100

    bien

--- src/personnages/Druide.java
+++ src/personnages/Druide.java
@@ -28,7 +28,7 @@ public class Druide {
 			resultat = random.nextInt(effetPotionMax);
 		}
 		forcePotion = resultat;
-		// hi bon bonjour h
+		// hi bon bonjour hh
 		if (forcePotion > 7) {
 			parler("J'ai préparé une super potion de force" + forcePotion);
 		} else {
